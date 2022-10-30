9b7be25658ace0a88cf4c34b2fa00ab6dd867373
KenMR <kmr5591@gmail.com>
2022-10-30 18:25:56 -0400

    Create basic function templates and test that add function works properly

---
+++ calculator.js
@@ -0,0 +1,18 @@
+
+// Basic functins for various calculator operations
+
+function add (a, b) {
+    return a + b;
+}
+
+/*function subtract (a, b) {
+
+}
+
+function multiply (a, b) {
+
+}
+
+function divide (a, b) {
+
+}*/
