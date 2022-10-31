929aaaf9c1a25b07adae6c4d3f5260cefc4f9d20
KenMR <kmr5591@gmail.com>
2022-10-31 19:28:00 -0400

    Add functions to fill array and calculator display when keys are pressed and clear on command

--- calculator.js
+++ calculator.js
@@ -1,3 +1,5 @@
+// array to hold values entered from the buttons
+let displayText = [];
 
 // Basic functions for various calculator operations
 
@@ -34,4 +36,42 @@ function operate (a, b, op) {
         result = divide(a, b);
     }
     return result;
+}
+
+function keyInput (entry) {
+    displayText.push(entry);
+    let div = document.getElementById('display');
+    if ((entry === 'add') || (entry === 'sub') || (entry === 'mul') || (entry === 'div')) {
+        switch(entry) {
+            case 'add':
+                div.innerHTML += '+';
+                break;
+            case 'sub':
+                div.innerHTML += '-';
+                break;
+            case 'mul':
+                div.innerHTML += 'x';
+                break;
+            case 'div':
+                div.innerHTML += '/';
+                break;
+        }
+    }
+    else {
+        div.innerHTML += entry;
+    }
+}
+
+function clearDisplay () {
+    // clears current display and all array elements
+    let div = document.getElementById('display');
+    div.innerHTML = '';
+    let len = displayText.length;
+    for (i = 0; i < len; i++) {
+    displayText[i] = null;
+    }
+}
+
+function calculate () {
+    // to be filled in
 }
